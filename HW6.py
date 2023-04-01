7e8243366e705381b2a33fa26885012a2e1d79e5
jjrich3 <102775600+jjrich3@users.noreply.github.com>
2023-03-31 21:04:56 -0400

    Add files via upload

---
+++ HW6.py
@@ -0,0 +1,233 @@
+import requests
+import json
+import unittest
+import os
+import re
+
+###########################################
+# Your name: Josh Richman                 #
+# Who you worked with:                    #
+###########################################
+
+def load_json(filename):
+    '''
+    Loads a JSON cache from filename if it exists
+
+    Parameters
+    ----------
+    filename: string
+        the name of the cache file to read in
+
+    Returns
+    -------
+    dict
+        if the cache exists, a dict with loaded data
+        if the cache does not exist, an empty dict
+    '''
+    try:
+        file = open(filename,"r")
+        cache = json.load(file)
+        file.close()
+        return cache
+    except:
+        return {}
+
+
+    pass
+
+def write_json(filename, dict):
+    '''
+    Encodes dict into JSON format and writes
+    the JSON to filename to save the search results
+
+    Parameters
+    ----------
+    filename: string
+        the name of the file to write a cache to
+    
+    dict: cache dictionary
+
+    Returns
+    -------
+    None
+        does not return anything
+    '''  
+    jsonFile = open(filename, 'w')
+    json.dump(dict, jsonFile)
+    jsonFile.close()
+
+    pass
+
+def get_swapi_info(url, params=None):
+    '''
+    Check whether the 'params' dictionary has been specified. Makes a request to access data with 
+    the 'url' and 'params' given, if any. If the request is successful, return a dictionary representation 
+    of the decoded JSON. If the search is unsuccessful, print out "Exception!" and return None.
+
+    Parameters
+    ----------
+    url (str): a url that provides information about entities in the Star Wars universe.
+    params (dict): optional dictionary of querystring arguments (default value is 'None').
+        
+
+    Returns
+    -------
+    dict: dictionary representation of the decoded JSON.
+    '''
+    if params != None:
+        req = requests.get(url,params=params)
+    else:
+        req = requests.get(url)
+    if req.ok:
+        jstring = req.json()
+        #print(jstring)
+        return jstring
+    else:
+        print("Exception!")
+    pass
+
+def cache_all_pages(people_url, filename):
+    '''
+    1. Checks if the page number is found in the dict return by `load_json`
+    2. If the page number does not exist in the dictionary, it makes a request (using get_swapi_info)
+    3. Add the data to the dictionary (the key is the page number (Ex: page 1) and the value is the results).
+    4. Write out the dictionary to a file using write_json.
+    
+    Parameters
+    ----------
+    people_url (str): a url that provides information about the 
+    characters in the Star Wars universe (https://swapi.dev/api/people).
+    filename(str): the name of the file to write a cache to
+        
+    '''
+    cache = load_json(filename)
+    #print(type(cache))
+    curr_url = people_url + "?page=1"
+    curr_page = get_swapi_info(curr_url)
+    #print(curr_page)
+    #page 2-9
+    i=1
+    while  type(curr_page) == dict and curr_page["next"] != None:
+        #print(cache.get("page "+str(i)))
+        if cache.get("page "+str(i)) and i < 9:
+            curr_url = people_url + "?page="+str(i+1)
+        else:
+            cache["page "+str(i)] = curr_page["results"]
+            if i==9:
+                continue
+            curr_page = get_swapi_info(curr_url)
+            curr_url = curr_page["next"]
+            
+            #print(type(curr_page))
+        #curr_url = curr_page["next"]
+        i -= -1
+    write_json(filename,cache)
+
+            
+
+    pass
+
+def get_starships(filename):
+    '''
+    Access the starships url for each character (if any) and pass it to the get_swapi_info function 
+    to get data about a person's starship.
+    
+    Parameter
+    ----------
+    filename(str): the name of the cache file to read in 
+    
+    Returns
+    -------
+    dict: dictionary with the character's name as a key and a list of the name their 
+    starships as the value
+    '''
+    cache = load_json(filename)
+    starships = {}
+    for page in cache:
+        #print(page)
+        for character in cache[page]:
+            #print(character)
+            if character["starships"]:
+                starships[character["name"]] = []
+                for ship in character["starships"]:
+                    ship_dic = get_swapi_info(ship)
+                    #print(ship_dic["name"])
+                    starships[character["name"]].append(ship_dic["name"])
+    return starships
+                
+                
+
+    pass
+
+#################### EXTRA CREDIT ######################
+
+def calculate_bmi(filename):
+    '''
+    Calculate each character's Body Mass Index (BMI) if their height and mass is known. With the metric 
+    system, the formula for BMI is weight in kilograms divided by height in meters squared. 
+    Since height is commonly measured in centimeters, an alternate calculation formula, 
+    dividing the weight in kilograms by the height in centimeters squared, and then multiplying 
+    the result by 10,000, can be used.
+
+    Parameter
+    ----------
+    filename(str): the name of the cache file to read in 
+    
+    Returns
+    -------
+    dict: dictionary with the name as a key and the BMI as the value
+    '''
+    char_bmis = {}
+    cache = load_json(filename)
+    for page in cache:
+        #print(page)
+        for character in cache[page]:
+            #print(character)
+            height = re.findall("[0-9.]+",character["height"])
+            weight = re.findall("[0-9.]+",character["mass"])
+            if height and weight:
+                character_bmi = int(height[0]) / (int(weight[0]) * int(weight[0]))
+                char_bmis[character["name"]] = character_bmi
+    print(char_bmis)
+    return char_bmis
+        
+
+    pass
+
+class TestHomework6(unittest.TestCase):
+    def setUp(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.filename = dir_path + '/' + "swapi_people.json"
+        self.cache = load_json(self.filename)
+        self.url = "https://swapi.dev/api/people"
+
+    def test_write_json(self):
+        write_json(self.filename, self.cache)
+        dict1 = load_json(self.filename)
+        self.assertEqual(dict1, self.cache)
+
+    def test_get_swapi_info(self):
+        people = get_swapi_info(self.url)
+        tie_ln = get_swapi_info("https://swapi.dev/api/vehicles", {"search": "tie/ln"})
+        self.assertEqual(type(people), dict)
+        self.assertEqual(tie_ln['results'][0]["name"], "TIE/LN starfighter")
+        self.assertEqual(get_swapi_info("https://swapi.dev/api/pele"), None)
+    
+    def test_cache_all_pages(self):
+        cache_all_pages(self.url, self.filename)
+        swapi_people = load_json(self.filename)
+        self.assertEqual(type(swapi_people['page 1']), list)
+
+    def test_get_starships(self):
+        starships = get_starships(self.filename)
+        self.assertEqual(len(starships), 19)
+        self.assertEqual(type(starships["Luke Skywalker"]), list)
+        self.assertEqual(starships['Biggs Darklighter'][0], 'X-wing')
+
+    def test_calculate_bmi(self):
+        bmi = calculate_bmi(self.filename)
+        self.assertEqual(len(bmi), 59)
+        self.assertAlmostEqual(bmi['Greedo'], 24.73)
+    
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
